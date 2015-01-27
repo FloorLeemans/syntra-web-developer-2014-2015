e9696069806548c117bfeee9823691ff27eb2c27
unknown <nosenzop@AKDGPORT6194.admin.kdg.be>
2015-01-27 17:21:23 +0100

    add examenopdracht & shellscript

---
+++ download-oplossingen.bat
@@ -0,0 +1,15 @@
+cd \_personal\syntra-14-15\web-backend\oplossingen-studenten
+
+git clone https://github.com/knopka-0075/web-backend-oplossingen.git olga
+git clone https://github.com/Christophvh/Oplossingen.git christoph
+git clone https://github.com/Feronium/web-backend.git simeon
+git clone https://github.com/jonasiliaens/web-backend-oplossingen.git jonas
+git clone https://github.com/leonieswart/oplossingen.git leonie
+git clone https://github.com/FloorLeemans/web-backend-oplossingen.git floor
+git clone https://github.com/YasminePlasmans/web-backend-oplossingen.git yasmine
+git clone https://github.com/GlenLauwers/web-backend-oplossingen.git glen
+git clone https://github.com/JoeriMahieu/web-backend-oplossingen.git joeri
+git clone https://github.com/mivixof/web-backend-solutions.git carl
+git clone https://github.com/Boran-Eksen/web-backend-oplossingen.git boran
+git clone https://github.com/l4wz/web-backend-oplossingen-LvH.git laurens
+
